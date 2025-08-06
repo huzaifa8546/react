136625170adc8c36dd03bd46de0242e7d5aeeffd
HUZAIFA VALIKARIMWALA <121677752+huzaifa8546@users.noreply.github.com>
2025-08-06 17:02:57 +0530

    feat: initial setup with Vite and React, added App and main files

--- 01basicvite/src/main.jsx
+++ 01basicvite/src/main.jsx
@@ -1,10 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import React from "react";
+const reactElement = {
+  type: "a",
+  props: {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  children: "Click Me To Visit The Google",
+};
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+function MyApp() {
+  return (
+    <div>
+      <h1>custom react app</h1>
+    </div>
+  );
+}
+
+const AnotherElement = (
+  <a href="http://google.com" target="_blank">
+    Visit Google
+  </a>
+);
+
+const AreactElement = React.createElement(
+  "a",
+  {
+    href: "https://google.com",
+    target: "_blank",
+  },
+  "Click to visit google"
+);
+createRoot(document.getElementById("root")).render(
+  // <AnotherElement />
+//   AreactElement
+  <App />
+);
