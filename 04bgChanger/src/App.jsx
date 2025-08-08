aa31bf8df1f05332064595c7f52850d6dd623bc6
HUZAIFA VALIKARIMWALA <121677752+huzaifa8546@users.noreply.github.com>
2025-08-08 16:17:20 +0530

    A Color changer app using vite and react

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [color, setColor] = useState("olive");
+  // function changeColor(color) {
+  //   setColor(color);
+  // }
+
+  return (
+    <div
+      class="w-full h-screen flex itmes-center justify-center duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div class="fixed flex flex-wrap justify-center bottom-13 inset-x-0 px-3">
+        <div class="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-4 py-3 rounded-3xl">
+          <button
+            onClick={() => setColor("red")}
+            class="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 shadow-lg shadow-red-500/50 dark:shadow-lg dark:shadow-red-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          >
+            Red
+          </button>
+          <button
+            onClick={() => setColor("green")}
+            class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          >
+            Green
+          </button>
+          <button
+            onClick={() => setColor("blue")}
+            class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 "
+          >
+            Blue
+          </button>
+
+          <button
+            onClick={() => setColor("purple")}
+            class="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          >
+            Purple
+          </button>
+
+          <button
+            onClick={() => setColor("lime")}
+            class="text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-lime-300 dark:focus:ring-lime-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          >
+            Lime
+          </button>
+
+          <button
+            onClick={() => setColor("cyan")}
+            class="text-white bg-gradient-to-r from-cyan-400 via-cyan-500 to-cyan-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+          >
+            Cyan
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
