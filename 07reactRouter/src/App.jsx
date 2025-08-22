b8cd9f6bbc089a12ad61317ca9a20f0aea866390
HUZAIFA VALIKARIMWALA <121677752+huzaifa8546@users.noreply.github.com>
2025-08-22 17:29:48 +0530

    A React project to understand routings

---
+++ 07reactRouter/src/App.jsx
@@ -0,0 +1,10 @@
+
+import "./App.css";
+
+function App() {
+  
+
+  return <div className="bg-red-300">React Router Dom</div>;
+}
+
+export default App;
---
+++ 07reactRouter/src/main.jsx
@@ -0,0 +1,38 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import Layout from "./Layout";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Contact from "./components/Contact/Contact";
+import User from "./components/User/User";
+import Github, { githubInfoLoader } from "./components/Github/Github";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route path="" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+
+      <Route path="user/" element={<User />}>
+        <Route path=":userid" element={<User />} />
+      </Route>
+      <Route loader={githubInfoLoader} path="github" element={<Github />} />
+
+      <Route path="*" element={<div>Not Found</div>} />
+    </Route>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
